76394b217bf55c20a635ba8e93fffe218493c3a3
Alex5531223 <113677365+Alex5531223@users.noreply.github.com>
2022-09-26 07:46:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from binance.client import Client
 from binance.enums import *
 
 app = Flask(__name__)
-symbol = 'BTCUSDT'
+symbol = 'BTCBUSD'
 api_key = '12d15a558ea00f21a1526acdea6c34b12974deb44e3d2a66675c8c19a8188163'
 api_secret = 'fabdda6763a3a539ea316b9fb149e9711c6b04d933a826c28e1fee94351aa178'
 client = Client(api_key, api_secret, testnet=True)
