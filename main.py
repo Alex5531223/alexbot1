dd2059b46b60e3133703afbb099993c2878b6d33
Alex5531223 <113677365+Alex5531223@users.noreply.github.com>
2022-09-26 08:17:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,9 @@ api_key = '12d15a558ea00f21a1526acdea6c34b12974deb44e3d2a66675c8c19a8188163'
 api_secret = 'fabdda6763a3a539ea316b9fb149e9711c6b04d933a826c28e1fee94351aa178'
 client = Client(api_key, api_secret, testnet=True)
 
-
 @app.route("/")
 def helloworld():
     return 'hello world'
-
-
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
@@ -26,21 +23,22 @@ def webhook():
     SIZE=data["SIZE"]
     LEVERAGE = data["LEV"]
     orederID=data["orderID"]
-    print(LEVERAGE)
+    Position_size=data["position_size"]
+    #print(LEVERAGE)
     client.futures_change_leverage(symbol=symbol, leverage=3)
 
 
     client.futures_cancel_all_open_orders(symbol=symbol)
     #client.future_can
 
-    if orederID=="BUY":
+    if orederID=="BUY" and Position_size=="0":
         buyorder = client.futures_create_order(symbol=symbol, side='BUY', type='LIMIT', quantity=SIZE, price=Etryprice , timeInForce='GTC')
 
         stoporder = client.futures_create_order(symbol=symbol, side='SELL', type='STOP_MARKET', quantity=SIZE, stopPrice=STprice)
 
         profitorder = client.futures_create_order(symbol=symbol, side='SELL', type='TAKE_PROFIT', quantity=SIZE, price=TPprice, stopPrice=TPprice)
 
-    if orederID=="SELL":
+    if orederID=="SELL" and Position_size=="0":
 
         buyorder = client.futures_create_order(symbol=symbol, side='SELL', type='LIMIT', quantity=SIZE, price=Etryprice ,timeInForce='GTC')
 
@@ -49,15 +47,12 @@ def webhook():
         profitorder = client.futures_create_order(symbol=symbol, side='BUY', type='TAKE_PROFIT', quantity=SIZE, price=TPprice, stopPrice=TPprice)
 
 
-
-
-    # print(buyorder)
+    #print(Position_size)
     #print(data["strategy"]["order_action"])
     return {
         "code": "success",
         "massage": data
     }
-
 
 if __name__ == "__main__":
     app.run(debug=True)
