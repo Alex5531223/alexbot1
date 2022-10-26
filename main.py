bf1dd2e3a3a7f0d7cf4296a76edf6dc986012958
Alex5531223 <113677365+Alex5531223@users.noreply.github.com>
2022-10-26 15:39:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def webhook():
 
         client.futures_cancel_all_open_orders(symbol=symbol)
 
-        buyorder = client.futures_create_order(symbol=symbol, side='BUY', type='LIMIT', quantity=SIZE, price=EN_long, timeInForce='GTX')
+        buyorder = client.futures_create_order(symbol=symbol, side='BUY', type='LIMIT', quantity=SIZE, price=EN_long, timeInForce='GTC', postOnly=True)
 
         stoporder = client.futures_create_order(symbol=symbol, side='SELL', type='STOP_MARKET', quantity=SIZE, stopPrice=STprice)
 
@@ -59,7 +59,7 @@ def webhook():
 
         client.futures_cancel_all_open_orders(symbol=symbol)
 
-        buyorder = client.futures_create_order(symbol=symbol, side='SELL', type='LIMIT', quantity=SIZE, price=EN_short, timeInForce='GTX')
+        buyorder = client.futures_create_order(symbol=symbol, side='SELL', type='LIMIT', quantity=SIZE, price=EN_short, timeInForce='GTC', postOnly=True)
 
         stoporder = client.futures_create_order(symbol=symbol, side='BUY', type='STOP_MARKET', quantity=SIZE, stopPrice=STprice)
 
